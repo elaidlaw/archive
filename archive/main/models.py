797a4d54dd58c51f3b645a41764c5350f59dc94d
domport <domport@users.noreply.github.com>
2020-03-17 15:09:01 -0400

    Update models.py

--- archive/main/models.py
+++ archive/main/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
-class Photo(model.Models):
+class Photo(models.Model):
     image_field = models.CharField(max_length=30)
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     description = models.CharField(max_length=1000)
     location = models.ForeignKey(Location, on_delete=models.SET_NULL)
 
-class Person(model.Models):
+class Person(models.Model):
     name = models.CharField(max_length=40)
 
-class Location(model.Models):
+class Location(models.Model):
     location = models.CharField(max_length=40)
 
-class Tag(model.Models):
+class Tag(models.Model):
     tag = models.CharField(max_length=40)
 
-class PersonInPhoto(model.Models):
+class PersonInPhoto(models.Model):
     photo = models.ForeignKey(Photo, on_delete=models.SET_NULL)
     person = models.ForeignKey(Person, on_delete=models.SET_NULL)
 
-class TagsOnPhoto(model.Models):
+class TagsOnPhoto(models.Model):
     photo = models.ForeignKey(Photo, on_delete=models.SET_NULL)
     tags = models.ForeignKey(Tag, on_delete=models.SET_NULL)
 
