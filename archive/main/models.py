ef49b18a9f080d566501b676d768be1d4853b358
domport <domport@users.noreply.github.com>
2020-03-17 15:08:18 -0400

    Update models.py

--- archive/main/models.py
+++ archive/main/models.py
@@ -5,12 +5,13 @@ class Photo(model.Models):
     start_date = models.DateTimeField('start date')
     end_date = models.DateTimeField('end date')
     description = models.CharField(max_length=1000)
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL)
 
 class Person(model.Models):
     name = models.CharField(max_length=40)
 
 class Location(model.Models):
-    location = models.CharField(max_length=20)
+    location = models.CharField(max_length=40)
 
 class Tag(model.Models):
     tag = models.CharField(max_length=40)
@@ -18,10 +19,6 @@ class Tag(model.Models):
 class PersonInPhoto(model.Models):
     photo = models.ForeignKey(Photo, on_delete=models.SET_NULL)
     person = models.ForeignKey(Person, on_delete=models.SET_NULL)
-
-class LocationOfPhoto(model.Models):
-    photo = models.ForeignKey(Photo, on_delete=models.SET_NULL)
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL)
 
 class TagsOnPhoto(model.Models):
     photo = models.ForeignKey(Photo, on_delete=models.SET_NULL)
